7162a030bfbfe9592f5d30c3b89a8988e7ef9432
César Fernández <kaesar84@icloud.com>
2023-09-05 19:41:42 +0200

    defineStyle

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <link rel="stylesheet" href="styles/css/styles.css">
+</head>
+<section class="hero is-primary ">
+    <div class="hero-body ">
+        <p class="title">
+            Link hero
+        </p>
+        <p class="subtitle">
+            Link subtitle
+        </p>
+    </div>
+</section>
+
+<body>
+    <section class="codeContainer">
+        <div class="container ">
+            <div class="columns">
+                <div class="column"></div>
+                <div class="column is-four-fifths">
+                    <div class="container codeWindow">
+                        <div class="codeNav">🔴🟡🟢</div>
+                        <div class="codeContent">
+                        <h5>titulo tr</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, nunc euismod ut fames odio scelerisque condimentum mauris, hac laoreet habitant imperdiet felis ultricies. Conubia curabitur fringilla litora scelerisque molestie habitasse curae cras cursus faucibus netus velit non, posuere ultrices porta class magnis aenean sem nec aliquam odio vestibulum nisl. Ligula lobortis aliquam ornare vitae sagittis pulvinar velit integer, mi tincidunt molestie sollicitudin arcu aptent et aenean, convallis himenaeos faucibus volutpat taciti odio montes.</p>    
+                        </div>
+                    </div>
+                </div>
+                <div class="column"></div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="resultContainer">
+        <div class="container ">
+            <div class="columns">
+                <div class="column"></div>
+                <div class="column is-four-fifths">
+                    <div class="container resutlWindow">
+                        <div class="resultNav">🔴🟡🟢</div>
+                        <div class="resultContent">
+                        <h5>titulo tr</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, nunc euismod ut fames odio scelerisque condimentum mauris, hac laoreet habitant imperdiet felis ultricies. Conubia curabitur fringilla litora scelerisque molestie habitasse curae cras cursus faucibus netus velit non, posuere ultrices porta class magnis aenean sem nec aliquam odio vestibulum nisl. Ligula lobortis aliquam ornare vitae sagittis pulvinar velit integer, mi tincidunt molestie sollicitudin arcu aptent et aenean, convallis himenaeos faucibus volutpat taciti odio montes.</p>    
+                        </div>
+                    </div>
+                </div>
+                <div class="column"></div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+</body>
+
+</html>
